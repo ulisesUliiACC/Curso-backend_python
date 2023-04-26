7947250de886cecdb0dbcc03fb44ade0c9171b51
Gabriel Ulises Martinez Gutierrez <ulisesuliiuwu@gmail.com>
2023-04-25 22:49:49 -0600

    uso de rutas para llamar archivos en el archivo main

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from   fastapi import  FastAPI
 
+from routers import products,users
+
 app = FastAPI()
+
+#routers  importacion
+#para llamar desde el main del proyecto 
+app.include_router(products.router)
+app.include_router(users.router)
 
 @app.get("/")
 def  root():
     return "hola ulises"
+
+@app.get("/url")
+async def url():
+    return {"url": "https://github.com/ulisesUliiACC"}
